7e75ccca337f5a9532ef9b381307e708361e3b48
Achmad Septiyandi <septiyandiole16@gmail.com>
2019-01-18 12:42:43 +0700

    pertemuan 5 & 6

--- application/controllers/admin/Produk.php
+++ application/controllers/admin/Produk.php
@@ -3,6 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Produk extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		if(empty($this->session->userdata('username')))
+		{
+			redirect('admin/login');
+		}
+	}
 	public function index()
 	{
 		$this->load->model('produk_model');
---
+++ application/models/admin/login_model.php
@@ -0,0 +1,15 @@
+<?php 
+
+class login_model extends CI_Model
+{
+	public $username;
+	public $password;
+
+	public function get_user_login($username, $password)
+	{
+		$filter = ['username' => $username, 'password' => $password];
+		$query = $this->db->get_where('admin', $filter);
+
+		return $query->result();
+	}
+}
